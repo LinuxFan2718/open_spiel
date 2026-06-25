c9ba64df3e561e55f2de23313dc12867dff3528a
Dennis Cahillane <denniscahillane@meta.com>
2026-06-25 06:58:06 -0700

    Revert word_tile commits from master
    Revert "play_word_tile: random seed by default, keep --seed for reproducibility"
    Revert "word_tile: implement core Scrabble gameplay logic"
    Revert "word_tile wip"
    
    The word_tile game development belongs on the two_player_word_tile branch,
    not master. The full implementation remains available at:
    https://github.com/LinuxFan2718/open_spiel/tree/two_player_word_tile
    
    This reverts commits cd993be3, 37a630a4, 4941fce6.

--- open_spiel/python/pybind11/pyspiel.cc
+++ open_spiel/python/pybind11/pyspiel.cc
@@ -55,7 +55,6 @@
 #include "open_spiel/python/pybind11/games_crazyhouse.h"
 #include "open_spiel/python/pybind11/games_crossword.h"
 #include "open_spiel/python/pybind11/games_dots_and_boxes.h"
-#include "open_spiel/python/pybind11/games_word_tile.h"
 #include "open_spiel/python/pybind11/games_euchre.h"
 #include "open_spiel/python/pybind11/games_gin_rummy.h"
 #include "open_spiel/python/pybind11/games_go.h"
@@ -856,7 +855,6 @@ PYBIND11_MODULE(pyspiel, m) {
   init_pyspiel_games_chess(m);
   init_pyspiel_games_crazyhouse(m);
   init_pyspiel_games_crossword(m);
-  init_pyspiel_games_word_tile(m);
   init_pyspiel_games_colored_trails(m);
   init_pyspiel_games_connect_four(m);
   init_pyspiel_games_crazy_eights(m);
